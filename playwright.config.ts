ba271b4a15b5b744d5ead8a41b6e26beb835b472
dyudovich <yudovichml@gmail.com>
2026-03-16 21:49:52 +0100

    e2e playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './test/e2e',
   use: {
     baseURL: 'http://localhost:5173',
   },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox',  use: { ...devices['Desktop Firefox'] } },
+    { name: 'webkit',   use: { ...devices['Desktop Safari'] } },
+    { name: 'edge',     use: { ...devices['Desktop Edge'] } },
+  ],
   webServer: {
     command: 'npm run dev',
     url: 'http://localhost:5173',
